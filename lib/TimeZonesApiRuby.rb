0a5de4b4514a275897c3bb1adfe3b819678a2fe3
Niko Roberts <niko@tasboa.com>
2022-03-14 15:38:41 +1100

    initial commit

---
+++ lib/TimeZonesApiRuby.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "TimeZonesApiRuby/version"
+
+module TimeZonesApiRuby
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/TimeZonesApiRuby/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module TimeZonesApiRuby
+  VERSION = "0.1.0"
+end
